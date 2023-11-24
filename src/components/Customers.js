3bc49113b8f115a79f6155496267fe2146af2d29
Faraz Ahmed <masterdot707@gmail.com>
2023-11-24 09:01:00 +0500

    finalized table now making api call

--- src/components/Customers.js
+++ src/components/Customers.js
@@ -1,36 +1,49 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import Logo from "../assets/Logo.png";
 import CustomerLogo from "../assets/customersicon.svg";
 import CustomersTable from "./CustomersTable";
 
 const Customers = () => {
+  useEffect(() => {}, []);
+
   return (
     <div className="min-h-screen flex">
       {/* sidebar */}
-      <div className="w-72 bg-[#015249] h-full flex flex-col justify-start items-center fixed top-0 left-0 bottom-0">
-        <img className="w-32 h-32 object-contain" src={Logo} />
+      <div className="w-56 bg-[#015249] h-full flex flex-col justify-start items-center fixed top-0 left-0 bottom-0">
+        <img
+          className="w-32 h-32 object-contain"
+          src={Logo}
+          alt="logo for website "
+        />
 
         <div className="bg-[#043933] cursor-pointer hover:scale-110 duration-150 rounded-md py-2 w-fit px-6 flex flex-row justify-between gap-x-3 items-center">
-          <img src={CustomerLogo} className="w-5 h-5 object-contain" />
-          <p className="text-md uppercase text-white font-semibold">
+          <img
+            src={CustomerLogo}
+            className="w-5 h-5 object-contain"
+            alt="customer logo"
+          />
+          <p className="text-md uppercase text-white  font-semibold">
             Customers
           </p>
         </div>
       </div>
 
       {/* customers area */}
-      <div className="w-full ml-72">
+      <div className="w-full ml-56">
         <div className="w-full bg-white py-7 px-4">
           <p className="text-2xl uppercase text-black font-bold">Customers</p>
         </div>
 
-        <div className="bg-[#F3F3F3] w-full h-full px-4">
-          <div className="w-fit rounded-md text-white bg-from-[#57BC90] to-[#004B40] ">
-            sdadas
+        <div className="bg-[#F3F3F3] pt-10 w-full h-full px-4">
+          <div className="w-fit flex flex-row justify-center items-center py-2 px-4 gap-x-2 rounded-md text-white bg-gradient-to-r from-[#57BC90] to-[#004B40] ">
+            <p className="text-white ">+</p>
+            <p className="text-sm uppercase text-white font-normal">
+              Add Customers
+            </p>
           </div>
+          <CustomersTable />
         </div>
-        <CustomersTable />
       </div>
     </div>
   );
--- src/components/CustomersTable.js
+++ src/components/CustomersTable.js
@@ -1,59 +1,52 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import CustomerPic from "../assets/customerPic.png";
 
 const CustomersTable = () => {
+  useEffect(() => {}, []);
   return (
     <div>
       <div class="relative overflow-x-auto py-10">
         <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-          <thead class="text-xs bg-[#57BC90] text-[#015249] uppercase  dark:bg-gray-700 dark:text-gray-400">
-            <tr>
+          <thead class="text-xs mb-5 bg-[#6fc7a1]  text-[#015249] uppercase  dark:bg-gray-700 dark:text-gray-400">
+            <tr className="">
+              <th scope="col" class="px-6 py-3 rounded-l-lg"></th>
               <th scope="col" class="px-6 py-3">
-                Product name
+                Customer ID
               </th>
               <th scope="col" class="px-6 py-3">
-                Color
+                Customer Name
               </th>
               <th scope="col" class="px-6 py-3">
-                Category
+                Customer Email
               </th>
-              <th scope="col" class="px-6 py-3">
-                Price
-              </th>
+              <th scope="col" class="px-6 py-3 rounded-r-lg"></th>
             </tr>
           </thead>
           <tbody>
-            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+            <tr class="  bg-white border-b dark:bg-gray-800 dark:border-gray-700">
               <th
                 scope="row"
-                class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                class="px-6  py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
               >
-                Apple MacBook Pro 17"
+                <img
+                  src={CustomerPic}
+                  alt="customer pic"
+                  className="h-16 w-16 object-contain"
+                />
               </th>
-              <td class="px-6 py-4">Silver</td>
-              <td class="px-6 py-4">Laptop</td>
-              <td class="px-6 py-4">$2999</td>
-            </tr>
-            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-              <th
-                scope="row"
-                class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-              >
-                Microsoft Surface Pro
-              </th>
-              <td class="px-6 py-4">White</td>
-              <td class="px-6 py-4">Laptop PC</td>
-              <td class="px-6 py-4">$1999</td>
-            </tr>
-            <tr class="bg-white dark:bg-gray-800">
-              <th
-                scope="row"
-                class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-              >
-                Magic Mouse 2
-              </th>
-              <td class="px-6 py-4">Black</td>
-              <td class="px-6 py-4">Accessories</td>
-              <td class="px-6 py-4">$99</td>
+              <td class="px-6 py-4 ">0.00</td>
+              <td class="px-6 py-4 text-[#57BC90] underline cursor-pointer capitalize ">
+                abc123
+              </td>
+              <td class="px-6 py-4">abc@gmil.com</td>
+              <td class="px-6 py-4 flex flex-row gap-x-3 justify-center mt-3 items-end">
+                <button className="text-center bg-[#6fc7a1] px-3 rounded-md py-1 text-[#34614e] capitalize">
+                  edit
+                </button>
+                <button className="text-center bg-[#d99898]  px-3 rounded-md py-1 text-[#D80000] capitalize">
+                  Delete
+                </button>
+              </td>
             </tr>
           </tbody>
         </table>
