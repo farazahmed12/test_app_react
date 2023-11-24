8d9364eafae8573653c9150765fc6d347d9d6c82
Faraz Ahmed <masterdot707@gmail.com>
2023-11-24 15:11:42 +0500

    added redux presisit

--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,27 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import customerReducer from "./customerSlice.js";
+import { persistReducer, persistStore } from "redux-persist";
 
-const store = configureStore({
-  reducer: {
-    customer: customerReducer,
-  },
+// import storage from "localforage"; // defaults to localStorage for web
+import storage from "redux-persist/lib/storage";
+
+const rootReducer = combineReducers({
+  customer: customerReducer,
+  // Add other reducers here
 });
 
-export default store;
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["customer"],
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  // Add middleware, enhancers, etc. as needed
+});
+
+// export default store;
+export const persistor = persistStore(store);
